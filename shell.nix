91ccac8d63c69f78dac0422cc1deeaed18f53592
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2023-08-30 13:15:19 +0300

    updated nix-shell so, i dont have to use poetry

--- shell.nix
+++ shell.nix
@@ -1,14 +1,24 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import <nixpkgs> { };
 in
-  pkgs.mkShell {
-    name = "Python Dev Environment";
-    buildInputs = with pkgs; [
-      poetry
-    ];
-    shellHook = ''
-      poetry install
-      poetry shell
-      clear
-    '';
-  }
+pkgs.mkShell {
+  name = "Python Dev Environment";
+  # buildInputs = with pkgs; [
+  #   python311
+
+  #   poetry
+  # ];
+  # shellHook = ''
+  #   poetry install
+  #   poetry shell
+  #   clear
+  # '';
+  nativeBuildInputs = with pkgs; [
+    python310
+    python310Packages.pytube
+    python310Packages.openai-whisper
+  ];
+  shellHook = ''
+    echo Start developing ...
+  '';
+}
